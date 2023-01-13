9790c327e4350073989de698751c689c140607bc
Varun Kumar <varu1311@gmail.com>
2023-01-13 15:33:38 +0530

    feat: check for prime and armstrong

---
+++ primeAndArmstrong.js
@@ -0,0 +1,22 @@
+function isPrime(num) {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+        if(num % i === 0) return false;
+    }
+    return num > 1;
+}
+
+function isArmstrong(num){
+    const numberOfDigits = num.toString().length
+    let sum = 0;
+    let temp = num;
+    while (temp > 0) {
+        let remainder = temp % 10;
+        sum += remainder ** numberOfDigits;
+        temp = parseInt(temp / 10);
+    }
+    return sum == num
+}
+
+function isPrimeAndArmstrong(n){
+    return isPrime(n) && isArmstrong(n);
+}
